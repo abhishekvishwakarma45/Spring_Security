ba9ae65bd5a94d2a1e3422602bae6d12dbc447e5
Abhishek Vishwakarma <abhishekv6281@gmail.com>
2024-12-23 15:54:19 +0530

    final commit everything works

--- src/main/java/com/abhishek/security/SecurityConfig.java
+++ src/main/java/com/abhishek/security/SecurityConfig.java
@@ -3,9 +3,12 @@ package com.abhishek.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
@@ -27,7 +30,7 @@ public class SecurityConfig {
           private UserService userService;
 
           @Bean
-          DaoAuthenticationProvider authProvider() {
+          AuthenticationProvider authProvider() {
                     DaoAuthenticationProvider provider = new DaoAuthenticationProvider();
                     provider.setUserDetailsService(userService);
                     provider.setPasswordEncoder(passwordEncoder());
@@ -41,8 +44,15 @@ public class SecurityConfig {
                                         .requestMatchers("/login", "/register").permitAll()
                                         .anyRequest().authenticated());
                     http.formLogin(customizer -> customizer.loginPage("/login.html").permitAll());
+                    http.httpBasic(Customizer.withDefaults());
+                    http.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
 
                     return http.build();
           }
 
 }
+
+// itwill not work on browser beacuse of session sessionManagement in browser
+// when we enter password and username it creates a everytime new session to
+// work it disable form login and then try. in postman we can pass username
+// passsword in authorixation-> basic auth then pass and useername
